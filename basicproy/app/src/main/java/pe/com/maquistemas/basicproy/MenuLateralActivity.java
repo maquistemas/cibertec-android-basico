be0795bcac3fdc81ca61bb9ba494b4ebac27995b
maquistemas <maquistemas@gmail.com>
2019-06-22 06:10:57 -0500

    se configuro opcion de menu lateral

--- basicproy/app/src/main/java/pe/com/maquistemas/basicproy/MenuLateralActivity.java
+++ basicproy/app/src/main/java/pe/com/maquistemas/basicproy/MenuLateralActivity.java
@@ -1,10 +1,11 @@
 package pe.com.maquistemas.basicproy;
 
-import android.support.annotation.NonNull;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -52,7 +53,57 @@ public class MenuLateralActivity extends AppCompatActivity implements Navigation
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_drafts:
+//                fragment = new DraftsFragment();
+                break;
+            case R.id.nav_sent:
+//                fragment = new SentItemsFragment();
+                break;
+            case R.id.nav_trash:
+//                fragment = new TrashFragment();
+                break;
+            case R.id.nav_help:
+                intent = new Intent(this, MenuActivity.class);
+                break;
+            case R.id.nav_feedback:
+//                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            default:
+                fragment = new InboxFragment();
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
 }
